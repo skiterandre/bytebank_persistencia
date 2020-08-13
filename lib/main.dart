98e4d0304ccbd866a15326f3ac286133a6b95423
andre.couto <andre.couto@thomasgreg.com.br>
2020-08-13 10:58:18 -0300

    commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bytebank/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        )
+      ),
+      home:Dashboard(),
+    );
+  }
+}
+
